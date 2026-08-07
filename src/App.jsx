99149a1bc7d73ddb765681d7a2ea1978b7dd3be3
JocelDS <jauriguedelossantos@gmail.com>
2026-08-07 20:13:25 +0800

    feat: add resume PDF and update App and Buttons components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Navbar } from "@/layout/Navbar";
 import { Hero } from "@/sections/Hero";
 import { About } from "@/sections/About";
-import { Skills } from "@/sections/Skills";
 import { Projects } from "@/sections/Projects";
 import { Education } from "@/sections/Education";
 import { Certifications } from "@/sections/Certifications";
@@ -15,7 +14,6 @@ function App() {
       <main>
         <Hero />
         <About />
-        <Skills />
         <Projects />
         <Education />
         <Certifications />
